99dd11f1f583dbfee94fede21e9f410a0eef30e3
Luiz F Dias <luizfd@gmail.com>
2023-06-06 10:40:11 -0300

    Delete Category

--- backend/src/main/java/com/luizfd/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/luizfd/dscatalog/services/CategoryService.java
@@ -4,12 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.luizfd.dscatalog.dto.CategoryDTO;
 import com.luizfd.dscatalog.entities.Category;
 import com.luizfd.dscatalog.repositories.CategoryRepository;
+import com.luizfd.dscatalog.services.exceptions.DatabaseException;
 import com.luizfd.dscatalog.services.exceptions.ResourceNotFoundException;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -52,6 +55,19 @@ public class CategoryService {
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
 		}		
+	}
+	
+	@Transactional(propagation = Propagation.SUPPORTS)
+	public void delete(Long id) {
+		if (!categoryRepository.existsById(id)) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		try {
+			categoryRepository.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 
 }
