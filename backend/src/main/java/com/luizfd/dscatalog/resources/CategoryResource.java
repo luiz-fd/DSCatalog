0400ad0b01110cf482dc23783e124b5392a2075a
Luiz F Dias <luizfd@gmail.com>
2023-06-06 10:24:35 -0300

    Insert Category

--- backend/src/main/java/com/luizfd/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/luizfd/dscatalog/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package com.luizfd.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.luizfd.dscatalog.dto.CategoryDTO;
 import com.luizfd.dscatalog.services.CategoryService;
@@ -31,6 +35,14 @@ public class CategoryResource {
 		CategoryDTO dto = categoryService.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+		dto = categoryService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+
 
 }
