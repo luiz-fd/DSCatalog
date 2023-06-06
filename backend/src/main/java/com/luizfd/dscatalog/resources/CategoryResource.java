26f821857d2b4f62e933c7f84c0513ab2a777b84
Luiz F Dias <luizfd@gmail.com>
2023-06-06 08:32:44 -0300

    H2 database

--- backend/src/main/java/com/luizfd/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/luizfd/dscatalog/resources/CategoryResource.java
@@ -1,24 +1,26 @@
 package com.luizfd.dscatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.luizfd.dscatalog.entities.Category;
+import com.luizfd.dscatalog.dto.CategoryDTO;
+import com.luizfd.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
+	@Autowired
+	private CategoryService categoryService;
+	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L,"Books"));
-		list.add(new Category(2L,"Eletronics"));
+	public ResponseEntity<List<CategoryDTO>> findAll(){
+		List<CategoryDTO> list = categoryService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
---
+++ backend/src/main/java/com/luizfd/dscatalog/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.luizfd.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luizfd.dscatalog.dto.CategoryDTO;
+import com.luizfd.dscatalog.entities.Category;
+import com.luizfd.dscatalog.repositories.CategoryRepository;
+
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
+		List<Category> resultado = categoryRepository.findAll();
+		return resultado.stream().map(x -> new CategoryDTO(x)).toList();		
+	}
+
+}
