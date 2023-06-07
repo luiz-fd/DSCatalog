003e02bd51dffb31d7129a9c429e6ed047fc1ebc
Luiz F Dias <luizfd@gmail.com>
2023-06-07 15:21:53 -0300

    ProductRepositoryTests

---
+++ backend/src/test/java/com/luizfd/dscatalog/tests/Factory.java
@@ -0,0 +1,22 @@
+package com.luizfd.dscatalog.tests;
+
+import java.time.Instant;
+
+import com.luizfd.dscatalog.dto.ProductDTO;
+import com.luizfd.dscatalog.entities.Category;
+import com.luizfd.dscatalog.entities.Product;
+
+public class Factory {
+	
+	public static Product createProduct() {
+		Product product = new Product(1L, "GFDGDF", "sfdgdfgd", 10.0, "dgfsfg", Instant.parse("2023-05-02T14:00:00Z"));
+		product.getCategories().add(new Category(2L,"Electronics"));
+		return product;
+	}
+	
+	public static ProductDTO createProductDTO() {
+		Product product = createProduct();
+		return new ProductDTO(product, product.getCategories());
+	}
+
+}
