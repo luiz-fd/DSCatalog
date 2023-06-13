8e25c0069717b843fb7884cc2365611e901ea8c3
Luiz F Dias <luizfd@gmail.com>
2023-06-13 11:32:22 -0300

    Sign Up

--- backend/src/main/java/com/luizfd/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/luizfd/dscatalog/dto/UserInsertDTO.java
@@ -1,8 +1,12 @@
 package com.luizfd.dscatalog.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class UserInsertDTO extends UserDTO {
 
+	@NotBlank(message = "Campo obrigatório")
+	@Size(min = 8, message = "Deve ter no mínimo 8 caracteres")
 	private String password;
 
 	UserInsertDTO() {
--- backend/src/main/java/com/luizfd/dscatalog/repositories/RoleRepository.java
+++ backend/src/main/java/com/luizfd/dscatalog/repositories/RoleRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.luizfd.dscatalog.entities.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+	
+	Role findByAuthority(String authority);
 
 }
